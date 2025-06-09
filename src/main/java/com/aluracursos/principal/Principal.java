9988f36476c7950005da74d77346b463ec2dcad7
Juan-Matias <matiasponcexd@gmail.com>
2025-06-09 15:19:09 -0400

    feat: add new dateLocal function in Principal class and format date output

--- src/main/java/com/aluracursos/principal/Principal.java
+++ src/main/java/com/aluracursos/principal/Principal.java
@@ -7,6 +7,8 @@ import com.aluracursos.model.Episodio;
 import com.aluracursos.service.ConsumoAPI;
 import com.aluracursos.service.ConvierteDatos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -58,6 +60,23 @@ public class Principal {
                 .toList();
 
         episodios.forEach(System.out::println);
+
+        // Busqueda de episodios a partir de x aÑo
+        System.out.println("Indique el año del episodio : ");
+        var fecha = teclado.nextInt();
+        teclado.nextLine();
+
+
+        LocalDate fechaBusqueda = LocalDate.of(fecha,1,1);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                .forEach(e -> System.out.println(
+                        "Temporada " + e.getTemporada() +
+                                "Episodio " + e.getTitulo()+
+                                "Fecha de Lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
+                ));
     }
 
 
